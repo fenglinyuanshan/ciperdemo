51725be01f3395ad57ee8d1226d86510f5c92857
yang.qin <yang.qin@omnidata.io>
2018-11-02 18:09:11 +0800

    初始化项目

---
+++ src/main/java/com/example/ciperdemo/CiperdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.ciperdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiperdemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CiperdemoApplication.class, args);
+  }
+}
